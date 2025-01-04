280c2d064eb5b6afa634c1b12cff60db3c495a91
Александр Патрин <patrin0aleks@gmail.com>
2025-01-05 02:00:00 +0500

    feat(MainWindow): Отработка новой логики работы фронта

---
+++ CommonUIMethods.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PetProjectEducational
+{
+    internal class CommonUIMethods
+    {
+        #region Метод создает границу, занимающую все пространство.
+        /// <summary>
+        /// Создает границу, занимающую все пространство.
+        /// </summary>
+        /// <param name="borderThickness"> Толщина границы. </param>
+        /// <param name="borderBrush"> Цвет границы. </param>
+        /// <param name="backgroundColor"> Цвет фона внутри границы. </param>
+        /// <param name="paddingThickness"> Толщина отступа между границей и контентом внутри. </param>
+        /// <param name="marginThickness"> Толищна отступа между границей и внешним пространством. </param>
+        /// <returns> Граница, занимающая все пространство. </returns>
+        public static Border CreateStretchBorder(int borderThickness, 
+                                    Brush borderBrush, 
+                                    Brush backgroundColor, 
+                                    int paddingThickness,
+                                    int marginThickness)
+        {
+            return new Border()
+            {
+                BorderThickness = new Thickness(borderThickness),
+                BorderBrush = borderBrush,
+                Background = backgroundColor,
+                Padding = new Thickness(paddingThickness),
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Margin = new Thickness(marginThickness),
+            };
+        }
+        #endregion
+
+        #region Метод создает ячейку текста с максимально возможным размером шрифта на 1 строку.
+        /// <summary>
+        /// Создает ячейку текста с максимально возможным размером шрифта на 1 строку.
+        /// </summary>
+        /// <param name="text"> Текст. </param>
+        /// <param name="textColor"> Цвет текста. </param>
+        /// <param name="fontWeight"> Насыщенность шрифта. </param>
+        /// <returns></returns>
+        public static Viewbox CreateTextBlockWithOneString(string text, 
+                                                            Brush textColor,
+                                                            FontWeight fontWeight)
+        {
+            TextBlock textBlock = new TextBlock
+            {
+                Text = text,
+                Foreground = textColor,
+                TextAlignment = TextAlignment.Center,
+                FontWeight = fontWeight
+            };
+
+            Viewbox viewbox = new Viewbox();
+            viewbox.Child = textBlock;
+            return viewbox;
+        }
+        #endregion
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static PetProjectEducational.CommonUIMethods;
 
 namespace PetProjectEducational
 {
@@ -17,15 +18,57 @@ namespace PetProjectEducational
     public partial class MainWindow : Window
     {
         /// <summary>
-        /// 
-        /// = new Frame() - указал, чтобы не вызывать предупреждение CS8618
+        /// Frame, который используется для отображения различного контента.
+        /// "= new Frame()" - указал, чтобы не вызывать предупреждение CS8618.
         /// </summary>
         public static Frame MainFrameInstance {  get; private set; } = new Frame();
 
         public MainWindow()
         {
             InitializeComponent();
-            MainFrameInstance = MainFrame;
+            SetupGrid();
+        }
+
+        public void SetupGrid()
+        {
+            // Добавляем строки и столбцы
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            MainGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            MainGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            
+            // Создаем 3 Border и 3 TextBlock
+            var border1 = CreateStretchBorder(5, Brushes.Black, Brushes.Bisque, 5, 10);
+            var border2 = CreateStretchBorder(5, Brushes.Black, Brushes.Gray, 5, 10);
+            var border3 = CreateStretchBorder(5, Brushes.Black, Brushes.Azure, 50, 10);
+
+            var textBlock1 = CreateTextBlockWithOneString("Пример текста внутри Border #1", Brushes.Black, FontWeights.Bold);
+            var textBlock2 = CreateTextBlockWithOneString("Пример текста внутри Border #2", Brushes.DarkBlue, FontWeights.DemiBold);
+            var textBlock3 = CreateTextBlockWithOneString("Пример текста внутри Border #3", Brushes.Crimson, FontWeights.UltraBlack);
+
+            // Привязываем TextBlock к своим Border
+            border1.Child = textBlock1;
+            border2.Child = textBlock2;
+            border3.Child = textBlock3;
+
+            // Указываем позиции в Grid
+            Grid.SetRow(border1, 0);
+            Grid.SetColumn(border1, 1);
+            Grid.SetColumnSpan(border1, 2);
+
+            Grid.SetRow(border2, 1);
+            Grid.SetColumn(border2, 0);
+
+            Grid.SetRow(border3, 1);
+            Grid.SetColumn(border3, 1);
+
+            // Добавляем Border-ы в Grid
+            MainGrid.Children.Add(border1);
+            MainGrid.Children.Add(border2);
+            MainGrid.Children.Add(border3);
         }
     }
 }
